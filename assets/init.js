331bd5ab8643a7fa2bed7061f3ee7106eb3fe72d
Sandeep Prajapati <sandeepnewa@gmail.com>
2020-08-27 13:48:20 +1000

    Shopify theme

---
+++ assets/init.js
@@ -0,0 +1,37 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+
+
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+  });
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import 'owl.carousel';
+//import 'imports?jQuery=jquery!owl.carousel';
+
